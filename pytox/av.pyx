d2bdf5dee221af29c07a897873b02a4ab666d3fc
iphydf <iphydf@users.noreply.github.com>
2022-01-17 00:46:47 +0000

    refactor: Rewrite pytox using Cython.
    The C code was too brittle, too manual, and segfaulted all over the place
    if you hold it wrong. This is an attempt at making pytox more
    maintainable and safer, as well as giving it a better API.

---
+++ pytox/av.pyx
@@ -0,0 +1,6 @@
+# cython: language_level=3
+
+from pytox import core
+
+def foo():
+    pass
--- setup.py
+++ setup.py
@@ -1,72 +1,29 @@
-import os
-import shlex
-import subprocess  # nosec
 from distutils.core import Extension
 from distutils.core import setup
 
-TOXAV_TEST_CODE = b"""
-extern int toxav_new();
-int (*x)() = &toxav_new;
-int main(){}
-"""
-
-
-def supports_av(libs):
-    ldflags = shlex.split(os.environ.get("LDFLAGS", ""))
-    proc = subprocess.Popen(  # nosec
-        ["cc", "-xc", "-"] + ldflags + libs,
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-    # Wait for process to terminate; ignore stdout/stderr.
-    proc.communicate(TOXAV_TEST_CODE)
-
-    if os.path.exists("a.out"):
-        os.remove("a.out")
-        return True
-    return False
-
-
-sources = ["pytox/pytox.c", "pytox/core.c", "pytox/util.c"]
 libraries = [
-    "opus",
-    "sodium",
     "toxcore",
-    "vpx",
 ]
 cflags = [
-    "-Wall",
-    # "-Werror",
-    "-Wextra",
-    "-Wno-declaration-after-statement",
-    "-Wno-missing-field-initializers",
-    "-Wno-unused-parameter",
-    "-fno-strict-aliasing",
+    "-funsigned-char",
 ]
-
-if supports_av(["-ltoxcore"]):
-    sources.append("pytox/av.c")
-    cflags.append("-DENABLE_AV")
-elif supports_av(["-ltoxcore", "-ltoxav"]):
-    sources.append("pytox/av.c")
-    cflags.append("-DENABLE_AV")
-    libraries.append("toxav")
-else:
-    print("Warning: AV support not found, disabled.")
 
 setup(
     name="py-toxcore-c",
-    version="0.2.0",
+    version="0.3.0",
     description="Python binding for Tox the skype replacement",
-    author="Wei-Ning Huang (AZ)",
-    author_email="aitjcize@gmail.com",
+    author="Iphigenia Df",
+    author_email="iphydf@gmail.com",
     url="http://github.com/TokTok/py-toxcore-c",
     license="GPL",
     ext_modules=[
-        Extension("pytox",
-                  sources,
+        Extension("pytox.av",
+                  ["pytox/av.c"],
                   extra_compile_args=cflags,
-                  libraries=libraries)
+                  libraries=libraries),
+        Extension("pytox.core",
+                  ["pytox/core.c"],
+                  extra_compile_args=cflags,
+                  libraries=libraries),
     ],
 )
