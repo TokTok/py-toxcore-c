3d559259fe6db0a3bbe1c4296cda108482a18a5c
Restyled.io <commits@restyled.io>
2022-01-17 00:50:00 +0000

    Restyled by yapf

--- tools/gen_api.py
+++ tools/gen_api.py
@@ -37,7 +37,7 @@ def needs_space(l: str, r: str) -> bool:
 def untokenize(tokens: Tuple[str, ...]) -> str:
     line = []
     for i in range(len(tokens) - 1):
-        if tokens[i : i + 2] == ("void", ")"):
+        if tokens[i:i + 2] == ("void", ")"):
             break
         line.append(tokens[i])
         if needs_space(tokens[i], tokens[i + 1]):
@@ -74,9 +74,8 @@ def parse_params(tokens: Tuple[str, ...]) -> List[Tuple[List[str], str]]:
     return params
 
 
-def finalize_handler(
-    type_prefix: str, handlers: List[str], event: str, params: List[str]
-) -> None:
+def finalize_handler(type_prefix: str, handlers: List[str], event: str,
+                     params: List[str]) -> None:
     handlers[-1] += ":"
     self = ""
     array = ""
@@ -167,9 +166,8 @@ def gen_cython(lines: Sequence[str], fun_prefix: str) -> List[str]:
             extern.append(f"    ctypedef struct {tokens[2]}")
 
         # enums
-        if (tokens[:2] == ("typedef", "enum") or tokens[0] == "enum") and tokens[
-            -1
-        ] == "{":
+        if (tokens[:2] == ("typedef", "enum")
+                or tokens[0] == "enum") and tokens[-1] == "{":
             extern.append("")
             extern.append(f"    cpdef enum {tokens[-2]}:")
             state.append("enum")
@@ -177,12 +175,9 @@ def gen_cython(lines: Sequence[str], fun_prefix: str) -> List[str]:
             extern.append(f"        {tokens[0]},")
 
         # functions and callbacks
-        if (
-            "(" in tokens
-            and tokens[0].isidentifier()
-            and token_before("(", tokens).startswith(fun_prefix)
-            and tokens[0] != "typedef"
-        ):
+        if ("(" in tokens and tokens[0].isidentifier()
+                and token_before("(", tokens).startswith(fun_prefix)
+                and tokens[0] != "typedef"):
             extern.append(f"    cdef {untokenize_fun(tokens)}")
             if ";" not in tokens:
                 state.append("fun")
@@ -190,7 +185,7 @@ def gen_cython(lines: Sequence[str], fun_prefix: str) -> List[str]:
         if tokens[:2] == ("typedef", "void"):
             extern.append(f"    c{untokenize_fun(tokens)}")
 
-            event = tokens[2][len(fun_prefix) : -3]
+            event = tokens[2][len(fun_prefix):-3]
             params = list(tokens[3:])
 
             # TODO(iphydf): Handle this better (by checking whether we have a callback install
@@ -200,8 +195,7 @@ def gen_cython(lines: Sequence[str], fun_prefix: str) -> List[str]:
                     f"cdef void handle_{untokenize_fun((event,) + tokens[3:])}"
                 )
                 install_handlers.append(
-                    f"    {fun_prefix}callback_{event}(ptr, handle_{event})"
-                )
+                    f"    {fun_prefix}callback_{event}(ptr, handle_{event})")
             if ";" not in tokens:
                 state.append("callback")
             else:
@@ -235,14 +229,12 @@ def main() -> None:
         for line in src_fh.readlines():
             if line.startswith("cdef extern from"):
                 with open(api, "r") as api_fh:
-                    print(
-                        "\n".join(
-                            gen_cython(
-                                api_fh.readlines(),
-                                fun_prefix=os.path.split(api)[-1].split(".")[0] + "_",
-                            )
-                        )
-                    )
+                    print("\n".join(
+                        gen_cython(
+                            api_fh.readlines(),
+                            fun_prefix=os.path.split(api)[-1].split(".")[0] +
+                            "_",
+                        )))
             else:
                 print(line.rstrip())
 
