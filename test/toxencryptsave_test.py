ba68641a51f7e30b331c765dee6227f4b6d09313
Restyled.io <commits@restyled.io>
2024-01-07 00:24:53 +0000

    Restyled by yapf

--- test/toxencryptsave_test.py
+++ test/toxencryptsave_test.py
@@ -7,7 +7,8 @@ class ToxencryptsaveTest(unittest.TestCase):
     def test_encrypt_decrypt(self):
         with c.Tox_Pass_Key_Ptr(b"hello") as pk:
             self.assertNotEqual(pk.encrypt(b"hello world"), b"hello world")
-            self.assertEqual(pk.decrypt(pk.encrypt(b"hello world")), b"hello world")
+            self.assertEqual(pk.decrypt(pk.encrypt(b"hello world")),
+                             b"hello world")
 
 
 if __name__ == "__main__":
