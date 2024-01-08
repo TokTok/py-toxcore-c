4db508f864034184c54784c678a467d69a4aaaac
Restyled.io <commits@restyled.io>
2024-01-08 03:45:10 +0000

    Restyled by yapf

--- tools/stubgen.py
+++ tools/stubgen.py
@@ -15,7 +15,8 @@ PACKAGES = [
 ]
 
 
-def process_class(name: str, cls: list[str], imports: set[str], attr: object) -> None:
+def process_class(name: str, cls: list[str], imports: set[str],
+                  attr: object) -> None:
     for mem in dir(attr):
         mem_attr = getattr(attr, mem)
         if mem == "__enter__":
@@ -26,10 +27,8 @@ def process_class(name: str, cls: list[str], imports: set[str], attr: object) ->
             cls.append(
                 "    def __exit__(self, exc_type: type[BaseException] | None, exc_value: BaseException | None, exc_traceback: TracebackType | None) -> None: ..."
             )
-        elif (
-            mem == "__init__"
-            or "cython_function_or_method" in mem_attr.__class__.__name__
-        ):
+        elif (mem == "__init__"
+              or "cython_function_or_method" in mem_attr.__class__.__name__):
             doc = mem_attr.__doc__.split("\n")[0]
             if " -> " not in doc:
                 doc += " -> None"
@@ -50,9 +49,8 @@ def process_class(name: str, cls: list[str], imports: set[str], attr: object) ->
                 raise Exception((mem, mem_attr.__doc__, mem_attr.__class__))
 
 
-def process_type(
-    sym: str, attr: object, imports: set[str], classes: dict[str, list[str]]
-) -> None:
+def process_type(sym: str, attr: object, imports: set[str],
+                 classes: dict[str, list[str]]) -> None:
     if not isinstance(attr, type):
         return
 
@@ -76,9 +74,8 @@ def process_type(
         process_class(sym, cls, imports, attr)
 
 
-def process_value(
-    sym: str, attr: object, consts: list[str], classes: dict[str, list[str]]
-) -> None:
+def process_value(sym: str, attr: object, consts: list[str],
+                  classes: dict[str, list[str]]) -> None:
     if isinstance(attr, int):
         enum = tuple(c for c in classes.keys() if sym.startswith(c.upper()))
         if enum:
@@ -101,12 +98,14 @@ def process_package(out_dir: str, pkg: object, prefix: str, name: str) -> None:
             continue
         if isinstance(attr, pytox.common.__class__):
             continue
-        if hasattr(attr, "__module__") and f"{prefix}.{attr.__module__}" != name:
+        if hasattr(attr,
+                   "__module__") and f"{prefix}.{attr.__module__}" != name:
             continue
         attrs.append((sym, attr))
 
     consts: list[str] = []
-    classes: collections.OrderedDict[str, list[str]] = collections.OrderedDict()
+    classes: collections.OrderedDict[str, list[str]] = collections.OrderedDict(
+    )
     imports: set[str] = set()
     missing: set[str] = {
         "Tox_Conference_Number",
@@ -125,7 +124,8 @@ def process_package(out_dir: str, pkg: object, prefix: str, name: str) -> None:
     for sym, attr in attrs:
         process_value(sym, attr, consts, classes)
 
-    with open(os.path.join(out_dir, f"{name.replace('.', '/')}.pyi"), "w") as pyi:
+    with open(os.path.join(out_dir, f"{name.replace('.', '/')}.pyi"),
+              "w") as pyi:
         pyi.write(f"# {name}\n")
         for s in sorted(imports):
             pyi.write(s + "\n")
